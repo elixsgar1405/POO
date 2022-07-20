91ceea987bdaf506097628ecee62cfc321aa90a2
elixsgar1405 <eliase.flores08@gmail.com>
2022-07-20 17:19:53 -0500

    Updated

---
+++ 01.php
@@ -0,0 +1,28 @@
+<?php include 'includes/header.php';
+
+// Definir una clase
+class Producto {
+    public $nombre;
+    public $precio;
+    public $disponible;
+}
+
+$producto = new Producto();
+$producto->nombre = 'PC';
+$producto->precio = 1200;
+$producto->disponible = true;
+
+echo "<pre>";
+var_dump($producto);
+echo "</pre>";
+
+$producto2 = new Producto();
+$producto2->nombre = 'Monitor';
+$producto2->precio = 500;
+$producto2->disponible = true;
+
+echo "<pre>";
+var_dump($producto2);
+echo "</pre>";
+
+include 'includes/footer.php';

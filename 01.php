d8b6891980e0c37247e4e7cb5e941955d1f36b4c
elixsgar1405 <eliase.flores08@gmail.com>
2022-07-27 15:05:04 -0500

    Updated

--- 01.php
+++ 01.php
@@ -2,24 +2,33 @@
 declare(strict_types = 1);
 include 'includes/header.php';
 
-// Definir una clase
+// ENCAPSULACION
+
 class Producto {
+
+    // PUBLIC- SE PUEDE ACCEDER Y MODIFICAR DESDE CUALQUIER LUGAR (CLASE U OBJETO)
+    // PROTECTED - SE PUEDE ACCEDER SOLO EN LA CLASE
+    // PRIVATE - SOLO MIEMBROS DE LA MISMA CLASE PUEDEN ACCEDER A EL
+
+
     public function __construct(public string $nombre, public int $precio, public bool $disponible)
     {
+    }
+
+    public function mostrarProducto() {
+        echo "El nombre de producto es " . $this->nombre . " y su precio es de " . $this->precio;
     }
 }
 
 $producto = new Producto('Monitor', 3500, true);
-
-echo "El nombre de producto es " . $producto->nombre . " y su precio es de " . $producto->precio;
+$producto->mostrarProducto();
 
 echo "<pre>";
 var_dump($producto);
 echo "</pre>";
 
 $producto2 = new Producto('PC', 12000, true);
-
-echo "El nombre de producto es " . $producto2->nombre . " y su precio es de " . $producto2->precio;
+$producto2->mostrarProducto();
 
 echo "<pre>";
 var_dump($producto2);

7ff83e63e4dbded05056b67c77ac5340408de39d
elixsgar1405 <eliase.flores08@gmail.com>
2022-08-05 20:48:55 -0500

    Updated

--- 06.php
+++ 06.php
@@ -1,4 +1,23 @@
 <?php include 'includes/header.php';
 
+interface TransporteI {
+    public function getInfo() : string;
+    public function getRuedas() : int;
+}
+
+class Transporte implements TransporteI {
+    public function __construct(protected int $ruedas, protected int $capacidad)
+    {
+        
+    }
+
+    public function getInfo() : string {
+        return "El transporte tiene " . $this->ruedas . " ruedas " . " y una capacidad de " . $this->capacidad . " personas ";
+    }
+
+    public function getRuedas() : int {
+        return $this->ruedas;
+        }
+    }
 
 include 'includes/footer.php';
